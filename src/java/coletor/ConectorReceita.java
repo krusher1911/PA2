8764729655756dd28f27072485e943ed115d241b
ovatsug60@hotmail.com <ovatsug60@hotmail.com>
2016-10-02 22:41:47 -0300

    post funcionando

--- src/java/coletor/ConectorReceita.java
+++ src/java/coletor/ConectorReceita.java
@@ -28,28 +28,22 @@ public class ConectorReceita {
         String urlParameters = nomes_parametros[0] + "=" + parametros[0];
         
         for (int i = 1; i < parametros.length; i++) {
-            urlParameters = urlParameters + "&" + URLEncoder.encode(nomes_parametros[i], "UTF-8") + "=" +  URLEncoder.encode(parametros[i], "UTF-8");
+            urlParameters = urlParameters + "&" + URLEncoder.encode(nomes_parametros[i], "UTF-8") 
+                    + "=" +  URLEncoder.encode(parametros[i], "UTF-8");
         }
         
          
-        //add reuqest header
-        //con.setRequestMethod("POST");
+
         con.setRequestProperty("User-Agent", USER_AGENT);
         con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-        //con.setRequestProperty("Cookie", cookie.getName());
         System.out.println("cookie name: \"" + cookie.getName() + "\"");
 
-        // Send post request
+        // Enviar POST
         con.setDoOutput(true);
         DataOutputStream wr = new DataOutputStream(con.getOutputStream());
         wr.writeBytes(urlParameters);
         wr.flush();
         wr.close();
-
-        //int responseCode = con.getResponseCode();
-        System.out.println("\nSending 'POST' request to URL : " + url);
-        System.out.println("Post parameters : " + urlParameters);
-        //System.out.println("Response Code : " + responseCode);
 
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(con.getInputStream()));
